1849175848db63f7914da28bca07f99331b571b1
Shiro-nn <54745523+Shiro-nn@users.noreply.github.com>
2025-05-15 13:59:44 +0500

    refactor(bundle): переработка процесса сборки
    - Удален плагин для обработки внешних модулей Node.js.
    - Реализована генерация временного файла 'dist/build.ts' с оберткой для модуля.
    - Изменен формат выходного бандла с ESM на CJS.
    - Переработаны настройки сборки для использования нового временного файла.
    - Добавлено удаление временного файла после завершения процесса сборки.

--- bundle.ts
+++ bundle.ts
@@ -1,56 +1,37 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.25.4/mod.js";
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.1";
 
-/**
- * Marks all `node:*` and bare core modules as external
- * so Node's built‑in ESM loader handles them at runtime.
- */
-const externalCoreModulesPlugin = {
-  name: "external-core-modules",
-  setup(build: esbuild.PluginBuild) {
-    // List of Node’s built‑in module names you want to externalize
-    const builtins = [
-      "assert",
-      "buffer",
-      "child_process",
-      "crypto",
-      "dns",
-      "events",
-      "fs",
-      "http",
-      "https",
-      "os",
-      "path",
-      "stream",
-      "util", /* …etc… */
-    ];
-    // Build a regex that matches either `node:foo` or `foo`
-    const filter = new RegExp(
-      `^(?:node:(?:${builtins.join("|")})|(?:${builtins.join("|")}))$`,
-    );
+const file = Deno.readTextFileSync("main.ts");
+let write = "";
+let start = false;
+for (const line of file.split("\n")) {
+  if (!line.startsWith("import") && !start) {
+    write += "(async () => {\n";
+    start = true;
+  }
+  write += `${line}\n`;
+}
+if (start)
+write += "\n})().catch(err => { console.error(err); process.exit(1); });";
+Deno.writeTextFileSync("dist/build.ts", write);
 
-    build.onResolve({ filter }, (args: { path: string }) => ({
-      path: args.path,
-      external: true,
-    }));
-  },
-};
+try {
+  await esbuild.initialize();
 
-await esbuild.initialize();
+  await esbuild.build({
+    entryPoints: ["dist/build.ts"],
+    bundle: true,
+    format: "cjs",
+    platform: "node",
+    plugins: [
+      ...denoPlugins({
+        loader: "native",
+      }),
+    ],
+    outfile: "dist/out.js",
+  });
 
-await esbuild.build({
-  entryPoints: ["main.ts"],
-  bundle: true,
-  format: "esm",
-  platform: "node",
-  plugins: [
-    externalCoreModulesPlugin,
-    ...denoPlugins({
-      loader: "native",
-    }),
-  ],
-  external: ["encoding", "os"],
-  outfile: "dist/out.js",
-});
-
-esbuild.stop();
+  esbuild.stop();
+} finally {
+  Deno.removeSync("dist/build.ts");
+}
